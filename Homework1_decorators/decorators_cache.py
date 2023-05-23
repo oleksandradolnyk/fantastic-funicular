3273fc4af82951380830f48ea9be264da4f103f3
Oleksandra Dolnyk <oleksandra.dolnyk@gmail.com>
2023-05-24 00:27:26 +0200

    Initial code push

---
+++ Homework1_decorators/decorators_cache.py
@@ -0,0 +1,24 @@
+def cache(max_limit=64):
+    def internal(f):
+        @functools.wraps(f)
+        def deco(*args, **kwargs):
+            cache_key = (args, tuple(kwargs.items()))
+            if cache_key in deco._cache:
+                # переносимо в кінець списку
+                deco._cache.move_to_end(cache_key, last=True)
+                return deco._cache[cache_key]
+            result = f(*args, **kwargs)
+            # видаляємо якшо досягли ліміта
+            if len(deco._cache) >= max_limit:
+                 # видаляємо перший елемент
+                deco._cache.popitem(last=False)
+            deco._cache[cache_key] = result
+            return result
+        deco._cache = OrderedDict()
+        return deco
+    return internal
+
+def fetch_url(url, first_n=100):
+    """Fetch a given url"""
+    res = requests.get(url)
+    return res.content[:first_n] if first_n else res.content
