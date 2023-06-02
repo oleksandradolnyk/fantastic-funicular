fee426edff41789eeeabc4aee3bef5a4903741bc
Oleksandra Dolnyk <oleksandra.dolnyk@gmail.com>
2023-06-02 22:19:04 +0200

    Changes to decorator

--- Homework1_decorators/decorators_cache.py
+++ Homework1_decorators/decorators_cache.py
@@ -1,12 +1,8 @@
 import functools, psutil, requests
 from collections import OrderedDict
 
-def cache(max_limit=64):
-    try:
-        max_limit = int(max_limit)
-    except ValueError as e:
-        print(e)
-        exit(1)
+def cache(max_limit: int = 64):
+    """max_limit argument must be an integer"""
     if max_limit <= 0:
         print("Cache limit can not be 0 or less.")
         exit(1)
@@ -20,9 +16,8 @@ def cache(max_limit=64):
             result = f(*args, **kwargs)
             if len(deco._cache) >= max_limit:
                 lowest = min(deco._cache_counts, key=deco._cache_counts.get)
-                print('lowest:', lowest)
-                del deco._cache[lowest]
-                del deco._cache_counts[lowest]
+                deco._cache.pop(lowest)
+                deco._cache_counts.pop(lowest)
             deco._cache[cache_key] = result
             deco._cache_counts[cache_key] = 1
             return result
@@ -35,13 +30,13 @@ def process_memory(f):
     @functools.wraps(f)
     def internal(*args, **kwargs):
         process = psutil.Process()
-        f(*args, **kwargs)
-        mem = process.memory_info().rss / (1024 ** 2)
-        print("Used memory: ", mem)
-        return mem
+        result = f(*args, **kwargs)
+        used_memory = process.memory_info().rss / (1024 ** 2)
+        print("Used memory: ", used_memory)
+        return result
     return internal
 
-@cache(max_limit=input("Max cache limit: "))
+@cache(max_limit=int(input("Max cache limit: ")))
 @process_memory
 def fetch_url(url, first_n=50):
     """Fetch a given url"""
