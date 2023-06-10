f54703dc2fc374597d09602dbfb32dd7162ec47d
Oleksandra Dolnyk <oleksandra.dolnyk@gmail.com>
2023-06-10 22:38:48 +0200

    Changes for main code

--- Homework3_flask_and_api/main.py
+++ Homework3_flask_and_api/main.py
@@ -1,53 +1,49 @@
-import csv, random, string, requests
-import pandas as pd
+import csv, requests
 
 from faker import Faker
+
 from flask import Flask
+
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs
 
 app = Flask(__name__)
 
-faker_instance = Faker("uk_UA")
+fake = Faker("uk_UA")
 
 @app.route("/")
 def hello_world():
-    # view
     return "<p>Hello, World!!!!</p>"
 
-@app.route("/students/<int:counter>/")
-def generate_students(counter):
-    if not 0 <= counter <= 1000:
-        return "ERROR: should be in range [1, 1000]"
+@app.route("/students")
+@use_kwargs(
+    {
+        "number": fields.Int(
+            load_default=10, validate=validate.Range(1, 1000)
+        ),
+    },
+    location="query"
+)
+def generate_students(number):
     headers = ["first_name", "last_name", "email", "password", "birthday"]
+    data = []
     with open('data/students.csv', 'w') as file:
         writer = csv.DictWriter(file, fieldnames=headers)
         writer.writeheader()
-        for student in range(counter):
-            password = "".join(random.choices(string.ascii_lowercase +
-                                              string.ascii_uppercase +
-                                              string.digits +
-                                              string.punctuation,
-                                              k=random.randint(15, 30)))
-            year = random.randint(1963, 2005)
-            month = random.randint(1, 12)
-            if month == 2:
-                max_days = 28
-            elif month in [4, 6, 9, 11]:
-                max_days = 30
-            else:
-                max_days = 31
-            day = random.randint(1, max_days)
-            birthday = '{day}/{month}/{year}'.format(day=day, month=month, year=year)
-            writer.writerow({
-                "first_name": faker_instance.first_name(),
-                "last_name": faker_instance.last_name(),
-                "email": faker_instance.ascii_free_email(),
-                "password": password,
-                "birthday": birthday
-                })
-    data = pd.read_csv("data/students.csv")
-    return data.to_html()
+        for student in range(number):
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            email = fake.ascii_free_email()
+            password = fake.password(length=25)
+            birthday = fake.date_of_birth(minimum_age=18, maximum_age=65)
+            person = {"first_name": first_name,
+                      "last_name": last_name,
+                      "email": email,
+                      "password": password,
+                      "birthday": birthday}
+            data.append(person)
+        writer.writerows(data)
+    return data
 
 @app.route("/bitcoin_rate")
 @use_kwargs(
@@ -62,11 +58,9 @@ def generate_students(counter):
     location="query"
 )
 def get_bitcoin_value(currency, convert):
-    rates_result = requests.get(url="https://bitpay.com/api/rates").json()
+    rates_result = requests.get(url=f"https://bitpay.com/api/rates/{currency}").json()
     symbols_result = requests.get(url="https://bitpay.com/currencies").json()
-    for entity in rates_result:
-        if entity['code'] == currency:
-            rate = entity['rate']
+    rate = rates_result['rate']
     for entity in symbols_result['data']:
         if entity['code'] == currency:
             currency_symbol = entity['symbol']
