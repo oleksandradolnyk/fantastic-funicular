49967b5285180204eff046b73ec621c5aeca1b94
Oleksandra Dolnyk <oleksandra.dolnyk@gmail.com>
2023-06-18 23:18:58 +0200

    Name of class using capital letter, decimal module for more precise calculation

--- Homework6_django_and_classes/frange_iterator.py
+++ Homework6_django_and_classes/frange_iterator.py
@@ -1,10 +1,12 @@
-class frange:
+from decimal import Decimal
+
+class Frange:
     def __init__(self, start, stop=None, step=1.0):
         if stop is None:
-            start, stop = 0.0, float(start)
-        self.start = float(start)
-        self.stop = stop
-        self.step = step
+            start, stop = Decimal('0.0'), Decimal(str(start))
+        self.start = Decimal(str(start))
+        self.stop = Decimal(str(stop))
+        self.step = Decimal(str(step))
 
     def __iter__(self):
         return self
@@ -21,14 +23,14 @@ class frange:
 
 
 # Тести
-assert (list(frange(5)) == [0, 1, 2, 3, 4])
-assert (list(frange(2, 5)) == [2, 3, 4])
-assert (list(frange(2, 10, 2)) == [2, 4, 6, 8])
-assert (list(frange(10, 2, -2)) == [10, 8, 6, 4])
-assert (list(frange(2, 5.5, 1.5)) == [2, 3.5, 5])
-assert (list(frange(1, 5)) == [1, 2, 3, 4])
-assert (list(frange(0, 5)) == [0, 1, 2, 3, 4])
-assert (list(frange(0, 0)) == [])
-assert (list(frange(100, 0)) == [])
+assert (list(Frange(5)) == [0, 1, 2, 3, 4])
+assert (list(Frange(2, 5)) == [2, 3, 4])
+assert (list(Frange(2, 10, 2)) == [2, 4, 6, 8])
+assert (list(Frange(10, 2, -2)) == [10, 8, 6, 4])
+assert (list(Frange(2, 5.5, 1.5)) == [2, 3.5, 5])
+assert (list(Frange(1, 5)) == [1, 2, 3, 4])
+assert (list(Frange(0, 5)) == [0, 1, 2, 3, 4])
+assert (list(Frange(0, 0)) == [])
+assert (list(Frange(100, 0)) == [])
 
 print('SUCCESS!')
