02bb354d4422c3c054bcc05a74b802e96e115da3
oleksandra <96960541+oleksandradolnyk@users.noreply.github.com>
2023-07-22 16:07:38 +0200

    Formatting function (#7)

---
+++ Homework11_databases/numbers.py
@@ -0,0 +1,10 @@
+def new_format(number):
+    formatter_number = f'{int(number):,}'
+    return formatter_number.replace(",", ".")
+
+assert (new_format("1000000") == "1.000.000")
+assert (new_format("100") == "100")
+assert (new_format("1000") == "1.000")
+assert (new_format("100000") == "100.000")
+assert (new_format("10000") == "10.000")
+assert (new_format("0") == "0")
